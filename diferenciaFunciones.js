c8c048f4479c9c68135f074b308d968af41f1360
GuilleAMG <guillermoamunguiag@gmail.com>
2022-10-28 16:27:24 -0600

    Tipos de funciones.

---
+++ diferenciaFunciones.js
@@ -0,0 +1,19 @@
+// Funciones normales
+
+function obtenerDatos(id){
+    console.log(id)
+}
+
+// Funciones tipo Flecha ( => )
+
+const obtenerDatos2 = (id) =>{
+    console.log(id)
+}
+
+// Funciones tipo Flecha Resumida
+
+const obtenerDatos3 = (id) => console.log(id)
+
+obtenerDatos(17)
+obtenerDatos2(06)
+obtenerDatos3(18)
